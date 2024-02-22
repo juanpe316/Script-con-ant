c10e9e145175b3535547515d5aaf141f07b1132e
juanpe316 <89323489+juanpe316@users.noreply.github.com>
2024-02-22 01:56:33 +0100

    Add files via upload

---
+++ scriptAnt/src/scriptAnt/Prueba.java
@@ -0,0 +1,16 @@
+package scriptAnt;
+/**
+ * Clase de Prueba.
+ */
+public class Prueba {
+	/**
+	 * Método Main.
+	 * @param args Recibidos como argumentos.
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Prueba");
+
+	}
+
+}
